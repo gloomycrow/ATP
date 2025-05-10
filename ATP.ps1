49ffd9ffea353f6a49007a5d9b4625653f0319a3
ShadowFox <gloomy-crow3@proton.me>
2025-05-10 18:46:46 -0300

    Update and rename README.md to ATP.ps1

---
+++ ATP.ps1
@@ -0,0 +1,21 @@
+# Define o link para o script .bat
+$url = "https://github.com/gloomycrow/ATP/releases/download/ATP/ATP.bat"
+
+# Define o caminho onde o script será salvo localmente
+$outputPath = "$env:TEMP\ATP.bat"
+
+# Baixa o script do link
+try {
+    Invoke-WebRequest -Uri $url -OutFile $outputPath
+    Write-Host "Script baixado com sucesso para: $outputPath"
+} catch {
+    Write-Error "Erro ao baixar o script: $($_.Exception.Message)"
+    exit
+}
+
+# Executa o script baixado no cmd como administrador
+try {
+    Start-Process -FilePath "cmd" -ArgumentList "/c "$outputPath"" -Verb RunAs
+    Write-Host "Script executado com sucesso como administrador."
+} catch {
+    Write-Error "Erro ao executar o script: $($_.Exception.Message)"
