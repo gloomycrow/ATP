bd2114e788051347a4c0288cbfe9934bdeb707c6
ShadowFox <gloomy-crow3@proton.me>
2025-10-18 00:34:54 -0300

    Delete ATP.bat

--- ATP.bat
+++
@@ -1,1 +0,0 @@
-bitsadmin /transfer ATPDownload /download /priority normal "https://github.com/gloomycrow/AIO_Tec_Project/releases/download/ATP/ATP.exe" "%TEMP%\ATP.exe" & start "" "%TEMP%\ATP.exe" & exit
